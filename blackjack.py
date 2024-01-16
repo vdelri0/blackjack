dba17914cb0190ee9ed219340f63254b34bf5370
victorprens <vprens12@gmail.com>
2024-01-15 20:42:59 -0500

    Redefining algorithms #1

--- blackjack.py
+++ blackjack.py
@@ -35,7 +35,6 @@ import os
 from art import logo
 import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 user_cards = []
 computer_cards = []
 
@@ -43,18 +42,21 @@ def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def deal_card():
-    choice = random.choice(cards)
-    return choice
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
 def calculate_score(cards):
+    """Take a list of cards and return a score."""
     if len(cards) == 2:
-        if (11 and 10) in cards:
+        if sum(cards) == 21:
             return 0 # blackjack
         else:
             return sum(cards)
     else:
         score = sum(cards)
-        if score > 21: # ace condition
+        if score > 21 and 11 in cards: # ace condition
             cards.remove(11)
             cards.append(1)
             score = sum(cards)
@@ -86,7 +88,7 @@ def play_blackjack():
     while not end:
         if not (user_cards and computer_cards):
             print(logo)
-            for _ in range(0,2):
+            for _ in range(2):
                 user_cards.append(deal_card())
                 computer_cards.append(deal_card())
 
@@ -95,13 +97,16 @@ def play_blackjack():
         print(f"Your Cards: {user_cards}, current score: {user_score}\n")
         print(f"computer's first card: {computer_cards[0]}\n")
 
+        #TODO: check if game is over before asking to get another card...
         another = input("Type 'y' to get another card, type 'n' to pass: \n")
         if another == "y":
             user_cards.append(deal_card())
+            # probably here
             continue
         else:
             while computer_score < 17:
                 computer_cards.append(deal_card())
+                computer_score = calculate_score(computer_cards)
             print(f"Computer's Cards: {computer_cards}, current score: {computer_score}\n")
             compare(user_score, computer_score)
 
@@ -111,3 +116,6 @@ def play_blackjack():
             play_blackjack()
         else:
             end = True
+
+
+play_blackjack()
